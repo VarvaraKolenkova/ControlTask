6ffc5cf6a109a8604cc40aac029dd6fdacfc4c94
Varvara.Kolenkova <var.kolenkova@yandex.ru>
2022-08-22 16:22:14 +0300

    Сделали ссылки в файле README на код программы и блок-схему

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,15 +1,15 @@
 ﻿// Программа, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа.
 
-void SelectionArray(string[] array, string[]newarray)
+void SelectionArray(string[] array, string[]newArray)
 {
     for (int i = 0; i < array.Length; i++)
     {
-        if(array[i].Length<=3) newarray[i] = array[i];
-        Console.Write($"{newarray[i]} ");
+        if(array[i].Length<=3) newArray[i] = array[i];
+        Console.Write($"{newArray[i]} ");
     }
 }
 
 string[] arr = new string[9] {"оn", "1235", "the", "sun", "clock", "+30", "melody", "?", "yes"};
-string[] newarr = new string[arr.Length];
-SelectionArray(arr, newarr);
+string[] newArr = new string[arr.Length];
+SelectionArray(arr, newArr);
 
