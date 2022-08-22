a79dea36e14041548bba5464d2b92198c4acb9b2
Varvara.Kolenkova <var.kolenkova@yandex.ru>
2022-08-22 11:51:15 +0300

    Записали алгоритм решения

---
+++ Task/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа, которая из имеющкгося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа.
+
+void SelectionArray(string[] array, string[]newarray)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length<=3) newarray[i] = array[i];
+        Console.Write($"{newarray[i]} ");
+    }
+}
+
+string[] array = new string[9] {"оn", "1235", "the", "sun", "clock", "+30", "melody", "?", "yes"};
+string[] newarray = new string[array.Length];
+SelectionArray(array, newarray);
+
